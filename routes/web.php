7ad20bd29566093ccc5db423e0fed79add51d24b
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-09-01 20:25:17 +0530

    small updates

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,4 @@ Route::middleware(['web'])->controller(PatientController::class)->group(function
     Route::get('/logout', 'logout')->name('logout');  
 });
 Route::get('/prescription/download/{id}', [PatientController::class, 'prescriptionPDF'])->name('prescription.pdf');
-Route::get('/prescription/download/{id}', [PatientController::class, 'prescriptionHTML'])->name('prescription.html');
+//Route::get('/prescription/download/{id}', [PatientController::class, 'prescriptionHTML'])->name('prescription.html');
